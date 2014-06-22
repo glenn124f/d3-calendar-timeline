c8e436e3bbe0916b6e26d550ba31e2740804cdec
Svend Tofte <stofte@gmail.com>
2014-06-22 20:09:51 +0200

    Revert "Slight refactor"
    This reverts commit 2cfe3c169951f5cdf8db866d38d02e501d7d536b.

--- app.js
+++ app.js
@@ -1,156 +1,135 @@
+var Chart = function() {
+    var domain_start = moment('today');
+    var domain_end = moment();
+    
+};
 
 Date.prototype.getWeek = function() {
     var onejan = new Date(this.getFullYear(), 0, 1);
     return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
 }
 
-var data = (function() {
-    var md = function(n) {
-        return moment().hours(0).minutes(0).seconds(0).hours(-n*24).toDate();
-    };
-    return [
-        {start: md(42), end: md(30), label: 'first', color: 'blue', track: 0 },
-        {start: md(30), end: md(20), label: 'second', color: 'pink', track: 0 },
-        {start: md(20), end: md(13), label: 'third', color: 'yellow', track: 0 },
-        {start: md(13), end: md(10), label: 'fourth', color: 'green', track: 0 },
-        {start: md(10), end: md(9), label: 'fifth', color: 'black', track: 0 },
+// determine default domain
+var initialDomain = [];
 
-        {start: md(13), end: md(6), label: 'first', color: 'red', track: 1 },
-        {start: md(6), end: md(3), label: 'second', color: 'red', track: 1 },
-        {start: md(3), end: md(0), label: 'third', color: 'red', track: 1 },
-        {start: md(0), end: md(-20), label: 'fourth', color: 'red', track: 1 }
-    ];
-})();
-
-var Chart = function(trackdata, elmid) {
-    var self = this;
-
-    var calendarWidth = 1000;
-    var calendarHeight = 200;
-    var weekboxHeight = 60;
-
-    var domainDefault = {
-        start: moment().isoWeekday(1).hours(0).minutes(0).seconds(0).hours(-24*7).toDate(),
-        end: moment().isoWeekday(1).hours(0).minutes(0).seconds(0).hours(24*21).toDate()
-    };
-
-    var svg = d3.select('#' + elmid)
-                 .append('svg')
-                 .attr('width', calendarWidth)
-                 .attr('height', calendarHeight);
-
-    var xScale = d3.time.scale().range([0, calendarWidth]);
-    var yScale = d3.scale.linear().range([calendarHeight, 0]);
-
-    // sets default domain
-    xScale.domain([domainDefault.start, domainDefault.end]);
-    yScale.domain([0, 2]);
-
-    // utilize built in axis for the days line.
-    var daysLine = d3.svg.axis()
-        .scale(xScale)
-        .orient('top')
-        .ticks(d3.time.days, 1)
-        .tickPadding(0)
-        .tickFormat(function(d) { return (['S', 'M', 'T', 'O', 'T', 'F', 'L'])[d.getDay()]; })
-        .tickSize(0);
-
-    // initial appending needs to be done once only.
-    svg.append('g')
-        .attr('class', 'days-line')
-        .attr('transform', 'translate(0, ' + (calendarHeight - weekboxHeight - 5) + ')')
-        .call(daysLine);
-
-    // generates week boxes on the fly ...
-    var generateWeeks = function() {
-        var weeks = [];
-        var current = xScale.domain();
-        // assumes domain is week aligned to a monday
-        var start = moment(current[0]).hours(-14*24);
-        var end = moment(current[1]).hours(14*24);
-        while (start.isBefore(end)) {
-            weeks.push({
-                start: moment(start).toDate(),
-                end: moment(start).hours(7*24).toDate()
-            });
-            start.hours(7*24);
-        }
-        return weeks;
-    };
-
-    // binds tracks data once
-    svg.selectAll('rect.step')
-        .data(trackdata)
-        .enter().append('rect')
-            .attr('class', 'step')
-            .attr('x', function(d) { return xScale(d.start); })
-            .attr('y', function(d, i) { return d.track * 50 + 15; })
-            .attr('width', function(d) { return xScale(d.end) - xScale(d.start) - 4; })
-            .attr('height', 30)
-            .attr('fill', function(d) { return d.color; });
-
-    // call after setting domain, to update visuals
-    var update = function(options) {
-
-        var weekBoxes = generateWeeks();
-        var weekNrOffset = 90;
-
-        var weekElm = svg.selectAll('rect.week-boxes')
-            .data(weekBoxes)
-            .enter().append('g').attr('class', 'week-boxes');
-
-        weekElm.append('rect')
-            .attr('x', function(d) { return xScale(d.start); })
-            .attr('y', calendarHeight - weekboxHeight)
-            .attr('width', function(d) { return xScale(d.end) - xScale(d.start) - 4; })
-            .attr('height', weekboxHeight)
-            .attr('fill', '#eee');
-
-        weekElm.append('text')
-            .attr('x', function(d) { return xScale(d.start) + weekNrOffset; })
-            .attr('y', calendarHeight - weekboxHeight + 20)
-            .attr('width', function(d) { return xScale(d.end) - xScale(d.start) - 4; })
-            .attr('height', weekboxHeight)
-            .text(function(d) { return 'UGE ' + d.start.getWeek(); });
-
-        // axis panning from https://gist.github.com/phoebebright/3098488
-        if (options.panned) {
-            svg.selectAll('rect.step')
-                .transition()
-                .attr('x', function(d) { return xScale(d.start); });
-            
-            svg
-                .select('.days-line')
-                .transition()
-                .call(daysLine)
-                .selectAll('text')
-                .attr('x', 6);
-        } else {
-            // no transition on initial render
-            svg
-                .select('.days-line')
-                .call(daysLine)
-                .selectAll('text')
-                .attr('x', 6);
-        }
-    };
-
-    var panHandler = function(e) {
-        var left = e.target.id === 'pan-left';
-        var curr = xScale.domain();
-        var shift = (7*24*60*60*1000) * (left?1:-1);
-        curr[0].setTime(curr[0].getTime() - shift);
-        curr[1].setTime(curr[1].getTime() - shift);
-        xScale.domain(curr);
-
-        update({panned: true});
-    };
-
-    document.getElementById('pan-left').addEventListener('click', panHandler);
-    document.getElementById('pan-right').addEventListener('click', panHandler);
-
-    // call update to render initial weeks
-    update({});
+var md = function(n) { // makes dates ...
+    var now = new Date().getTime();
+    return new Date().setTime(now-(n*24*60*60*1000));
 };
 
-var chart = new Chart(data, 'calendar');
+var data = [
+    {start: md(42), end: md(30), label: 'first', color: 'blue', track: 0 },
+    {start: md(30), end: md(20), label: 'second', color: 'pink', track: 0 },
+    {start: md(20), end: md(13), label: 'third', color: 'yellow', track: 0 },
+    {start: md(13), end: md(10), label: 'fourth', color: 'green', track: 0 },
+    {start: md(10), end: md(9), label: 'fifth', color: 'black', track: 0 },
+
+    {start: md(13), end: md(6), label: 'first', color: 'red', track: 1 },
+    {start: md(6), end: md(3), label: 'second', color: 'red', track: 1 },
+    {start: md(3), end: md(0), label: 'third', color: 'red', track: 1 },
+    {start: md(0), end: md(-20), label: 'fourth', color: 'red', track: 1 }
+];
+var weekBoxes = [];
+
+var today = new Date();
+var weekMs = 7*24*60*60*1000;
+var fourWeeks = new Date();
+fourWeeks.setTime(today.getTime()-(4*weekMs));
+
+// generate week boxes
+var weekStart = new Date(d3.min(data, function(d) { return d.start; }));
+var weekEnd = new Date(d3.max(data, function(d) { return d.end; }));
+weekStart.setDate(weekStart.getDate()-weekStart.getDay()); // rewind to monday
+while(weekStart.getTime() < weekEnd.getTime()) {
+    weekBoxes.push({
+        start: new Date(weekStart),
+        end: new Date(weekStart.getTime()+weekMs),
+    })
+    weekStart.setTime(weekStart.getTime()+weekMs);
+}
+
+var width = 1000;
+var height = 200;
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
+
+x.domain([fourWeeks, today]);
+y.domain([0, 2]);
+
+var daysAxis = d3.svg.axis()
+    .scale(x)
+    .orient('top')
+    .ticks(d3.time.days, 1)
+    .tickPadding(0)
+    .tickFormat(function(d) { return (['M', 'T', 'O', 'T', 'F', 'L', 'S'])[d.getDay()]; })
+    .tickSize(0);
+
+var svg = d3.select("body")
+             .append("svg")
+             .attr("width", width)
+             .attr("height", height);
+
+svg.append('g')
+    .attr('class', 'x-axis')
+    .attr('transform', 'translate(0, ' + (height - 65) + ')')
+    .call(daysAxis);
+// shift days over a tiny bit ...
+svg.select('.x-axis').selectAll('text').attr('x','8');
+
+svg.selectAll('rect.bars')
+    .data(data)
+    .enter().append('rect')
+        .attr('class', 'bars')
+        .attr('x', function(d) { return x(d.start); })
+        .attr('y', function(d, i) { return d.track * 50 + 15; })
+        .attr('width', function(d) { return x(d.end) - x(d.start) - 4; })
+        .attr('height', 30)
+        .attr('fill', function(d) { return d.color; });
+
+var weekElm = svg.selectAll('rect.week-boxes')
+    .data(weekBoxes)
+    .enter().append('g').attr('class', 'week-boxes');
+
+weekElm.append('rect')
+    .attr('x', function(d) { return x(d.start); })
+    .attr('y', height-60)
+    .attr('width', function(d) { return x(d.end) - x(d.start) - 4; })
+    .attr('height', 60)
+    .attr('fill', '#eee');
+
+weekElm.append('text')
+    .attr('x', function(d) { return x(d.start) + 90; })
+    .attr('y', height-30)
+    .attr('width', function(d) { return x(d.end) - x(d.start) - 4; })
+    .attr('height', 60)
+    .text(function(d) { return 'UGE ' + d.start.getWeek(); });
+
+
+var panHandler = function(e) {
+    // updating data http://pothibo.com/2013/09/d3-js-how-to-handle-dynamic-json-data/
+    // axis panning from https://gist.github.com/phoebebright/3098488
+    var left = e.target.id === 'pan-left';
+    var curr = x.domain();
+    var shift = weekMs * (left?1:-1);
+    curr[0].setTime(curr[0].getTime() - shift);
+    curr[1].setTime(curr[1].getTime() - shift);
+    x.domain(curr);
+
+    svg.selectAll('rect.bars, g.week-boxes rect')
+        .transition()
+        .attr('x', function(d) { return x(d.start); });
+    
+    svg.selectAll('g.week-boxes text')
+        .transition()
+        .attr('x', function(d) { return x(d.start) + 90; });
+    
+    svg
+        .select('.x-axis')
+        .transition()
+        .call(daysAxis)
+        .selectAll('text').attr('x','8');
+
+};
+
+document.getElementById('pan-left').addEventListener('click', panHandler);
+document.getElementById('pan-right').addEventListener('click', panHandler);
